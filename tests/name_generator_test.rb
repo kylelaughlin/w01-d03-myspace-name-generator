2399227cb3ebb19f04013a6c3e32386630f2241c
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-16 15:14:46 -0500

    Add documentation to assert_equal and refute_equal methods

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -3,6 +3,14 @@ require_relative '../lib/name_generator.rb'
 # Define whatever methods you need first
 
 # Now run your actual tests, making sure to do output as necessary
+
+# Assert that the expected equals the actual, if not then display a message
+#
+# +expected: string representing what the method is expected to return
+# +actual: string representing what is returned by the method being tested
+# +message: string representing the text displayed when the test fails
+#
+# Returns "-" if successful and message if failed
 def assert_equal (expected, actual, message)
   if expected == actual
     puts "-"
@@ -11,6 +19,14 @@ def assert_equal (expected, actual, message)
   end
 end
 
+# Assert that the expected does not equal the actual, if it does then display
+#   a message.
+#
+# +expected: string representing what the method is expected to return
+# +actual: string representing what is returned by the method being tested
+# +message: string representing the text displayed when the test fails
+#
+# Returns "-" if successful and message if failed
 def refute_equal (expected, actual, message)
   if ! expected == actual
     puts "-"
